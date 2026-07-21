d9bf2c64fe88c7445bc46ffa411d60047a752096
karangupt <karangupt@gmail.com>
2026-07-21 18:15:25 +0530

    Set the Sheets Web App URL

--- js/sheets-api.js
+++ js/sheets-api.js
@@ -20,7 +20,7 @@
    fully usable, just single-device.
 */
 
-const SHEETS_WEB_APP_URL = ''; // <-- paste your Apps Script Web App URL here
+const SHEETS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyBYRHfdkl8LmPgp2rT3ancUwjsCT_y1IfZBJzN26HJOYqsP24qsz7WfYPxzjkoSgEB6Q/exec'; // <-- paste your Apps Script Web App URL here
 
 const SheetsAPI = (() => {
   const isConfigured = () => !!SHEETS_WEB_APP_URL;
